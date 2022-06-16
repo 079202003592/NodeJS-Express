dba979438c362da062e89a57bc6a44d81a2659e1
079202003592 <quan.nguyendong031102@hcmut.edu.vn>
2022-06-16 10:26:35 +0700

    add prettier config

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+    app.set('view engine', 
+    'hbs');
+  app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
